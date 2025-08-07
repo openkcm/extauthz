601a3ae872015a36ad1adfdd8c4f7e9ba646e105
Nicolae Nicora <n.nicora@sap.com>
2025-08-07 13:46:11 +0200

    fix: use the lowercase feature gates keys as values, as this is imposed by viper destabilising from the yaml config

--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -2,11 +2,11 @@ package flags
 
 const (
 	// DisableJWTTokenComputation disabling the existing JWT Handler, allowing the call to move forward
-	DisableJWTTokenComputation = "DisableJWTTokenComputation"
+	DisableJWTTokenComputation = "disable-jwt-token-computation"
 	// DisableClientCertificateComputation disabling the existing client certificates handler, allowing the call to move forward
-	DisableClientCertificateComputation = "DisableClientCertificateComputation"
+	DisableClientCertificateComputation = "disable-client-certificate-computation"
 	// EnrichHeaderWithClientRegion if set on true is including the client region. information in the headers
-	EnrichHeaderWithClientRegion = "EnrichHeaderWithClientRegion"
+	EnrichHeaderWithClientRegion = "enrich-header-with-client-region"
 	// EnrichHeaderWithClientType if set on true is including the client type. information in the headers
-	EnrichHeaderWithClientType = "EnrichHeaderWithClientType"
+	EnrichHeaderWithClientType = "enrich-header-with-client-type"
 )
