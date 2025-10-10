398abc73402e304a47d176fcae95faf8e8299883
Chris Burkert <burkert.chris@gmail.com>
2025-10-10 13:58:31 +0200

    chore: a few improvements (#110)

--- internal/business/extauthz.go
+++ internal/business/extauthz.go
@@ -14,40 +14,44 @@ import (
 )
 
 func createExtAuthZServer(ctx context.Context, cfg *config.Config) (*extauthz.Server, error) {
+	// prepare the options for the server
+	extauthzServerOptions := []extauthz.ServerOption{
+		extauthz.WithFeatureGates(&cfg.FeatureGates),
+	}
+
 	// Load the private key for signing the client data
 	clientDataFactory, err := clientdata.NewFactory(&cfg.FeatureGates, &cfg.ClientData)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create client data factory: %w", err)
 	}
-
 	if clientDataFactory.Enabled() {
 		slogctx.Info(ctx, "Using client data with signing key", "id", clientDataFactory.SigningKeyID())
 	} else {
 		slogctx.Info(ctx, "Using client data has been disabled")
 	}
+	extauthzServerOptions = append(extauthzServerOptions, extauthz.WithClientDataFactory(clientDataFactory))
 
 	// Load all Cedar policy files from the policy path
 	slogctx.Info(ctx, "Handling cedar policies", "cedar", cfg.Cedar)
-
 	pe, err := cedarpolicy.NewEngine(cedarpolicy.WithPath(cfg.Cedar.PolicyPath))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create the policy engine: %w", err)
 	}
+	extauthzServerOptions = append(extauthzServerOptions, extauthz.WithPolicyEngine(pe))
+
 	// Load the trusted subjects
 	subjects, err := loadTrustedSubjects(cfg.MTLS.TrustedSubjectsYaml)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load trusted subjects: %w", err)
 	}
-
 	if len(cfg.JWT.IssuerClaimKeys) == 0 {
 		slogctx.Warn(ctx, "JWT configuration doesn't have the issuer claims keys; Use the default values: [iss].")
-
 		cfg.JWT.IssuerClaimKeys = jwthandler.DefaultIssuerClaims
 	}
+	extauthzServerOptions = append(extauthzServerOptions, extauthz.WithTrustedSubjects(subjects))
 
 	// Create the JWT handler
 	slogctx.Debug(ctx, "Using k8s JWT providers", "k8s", cfg.JWT.K8sProviders)
-
 	hdl, err := jwthandler.NewHandler(
 		jwthandler.WithIssuerClaimKeys(cfg.JWT.IssuerClaimKeys...),
 		jwthandler.WithK8sJWTProviders(
@@ -61,15 +65,10 @@ func createExtAuthZServer(ctx context.Context, cfg *config.Config) (*extauthz.Se
 	if err != nil {
 		return nil, fmt.Errorf("failed to create the JWT handler: %w", err)
 	}
+	extauthzServerOptions = append(extauthzServerOptions, extauthz.WithJWTHandler(hdl))
 
 	// Create the ExtAuthZ server
-	srv, err := extauthz.NewServer(
-		extauthz.WithPolicyEngine(pe),
-		extauthz.WithJWTHandler(hdl),
-		extauthz.WithClientDataFactory(clientDataFactory),
-		extauthz.WithTrustedSubjects(subjects),
-		extauthz.WithFeatureGates(&cfg.FeatureGates),
-	)
+	srv, err := extauthz.NewServer(extauthzServerOptions...)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create the ExtAuthZ server: %w", err)
 	}
