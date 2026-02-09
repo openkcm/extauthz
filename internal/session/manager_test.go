e54793dc97ef05d8ddc0be78e842f6a1571ceecc
Danylo Shevchenko <danilshevchenko123456@gmail.com>
2026-02-09 13:57:46 +0100

    chore: increase test coverage

--- internal/session/manager_test.go
+++ internal/session/manager_test.go
@@ -2,9 +2,13 @@ package session
 
 import (
 	"context"
+	"errors"
 	"net"
+	"net/url"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"google.golang.org/grpc"
@@ -12,6 +16,9 @@ import (
 	"google.golang.org/grpc/test/bufconn"
 
 	sessionv1 "github.com/openkcm/api-sdk/proto/kms/api/cmk/sessionmanager/session/v1"
+	typesv1 "github.com/openkcm/api-sdk/proto/kms/api/cmk/types/v1"
+
+	"github.com/openkcm/extauthz/internal/oidc"
 )
 
 type SessionManagerMock struct {
@@ -26,6 +33,15 @@ func (m *SessionManagerMock) GetSession(ctx context.Context, req *sessionv1.GetS
 	}
 	//nolint:forcetypeassert
 	return args.Get(0).(*sessionv1.GetSessionResponse), args.Error(1)
+}
+
+func (m *SessionManagerMock) GetOIDCProvider(ctx context.Context, req *sessionv1.GetOIDCProviderRequest) (*sessionv1.GetOIDCProviderResponse, error) {
+	args := m.Called(ctx, req)
+	if args.Get(0) == nil {
+		return &sessionv1.GetOIDCProviderResponse{}, args.Error(1)
+	}
+	//nolint:forcetypeassert
+	return args.Get(0).(*sessionv1.GetOIDCProviderResponse), args.Error(1)
 }
 
 func startMockGRPCServer(t *testing.T) (*grpc.Server, *SessionManagerMock, *bufconn.Listener) {
@@ -107,8 +123,8 @@ func TestGetSession(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			// Arrange
-			manager, mock, terdown := setupSessionClient(t)
-			defer terdown()
+			manager, mock, teardown := setupSessionClient(t)
+			defer teardown()
 			if tc.setupMocks != nil {
 				tc.setupMocks(mock)
 			}
@@ -119,6 +135,69 @@ func TestGetSession(t *testing.T) {
 			// Assert
 			assert.NoError(t, err)
 			assert.Equal(t, tc.expected, got)
+			mock.AssertExpectations(t)
+		})
+	}
+}
+
+func TestManager_GetOIDCProvider(t *testing.T) {
+	issuerURL, _ := url.Parse("https://example.com/iss")
+	jwksURI, _ := url.Parse("https://example.com/jwks")
+	audiences := []string{"a", "b"}
+	provider, err := oidc.NewProvider(issuerURL, audiences, oidc.WithCustomJWKSURI(jwksURI))
+	if err != nil {
+		panic(err)
+	}
+
+	tests := []struct {
+		name       string
+		setupMocks func(*SessionManagerMock)
+		tenantID   string
+		want       *oidc.Provider
+		wantErr    bool
+	}{
+		{
+			name: "Success",
+			setupMocks: func(sm *SessionManagerMock) {
+				sm.On("GetOIDCProvider", mock.Anything, mock.Anything).Return(&sessionv1.GetOIDCProviderResponse{
+					Provider: &typesv1.OIDCProvider{
+						IssuerUrl: issuerURL.String(),
+						JwksUri:   jwksURI.String(),
+						Audiences: audiences,
+					},
+				}, nil)
+			},
+			tenantID: "tenant-id",
+			want:     provider,
+			wantErr:  false,
+		},
+		{
+			name: "Error",
+			setupMocks: func(sm *SessionManagerMock) {
+				sm.On("GetOIDCProvider", mock.Anything, mock.Anything).Return(nil, errors.New("some error"))
+			},
+			tenantID: "tenant-id",
+			want:     nil,
+			wantErr:  true,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			manager, mock, teardown := setupSessionClient(t)
+			defer teardown()
+			if tc.setupMocks != nil {
+				tc.setupMocks(mock)
+			}
+
+			got, err := manager.GetOIDCProvider(t.Context(), tc.tenantID)
+			if (err != nil) != tc.wantErr {
+				t.Fatalf("Manager.GetOIDCProvider() return an unexpected error %v", err)
+			}
+
+			if diff := cmp.Diff(tc.want, got, cmpopts.IgnoreUnexported(oidc.Provider{})); diff != "" {
+				t.Fatalf("Unexpected provider result (-want, +got):\n%s", diff)
+			}
+
 			mock.AssertExpectations(t)
 		})
 	}
