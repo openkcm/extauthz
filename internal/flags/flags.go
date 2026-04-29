6e676a262ace8dc0e5908b2cb7bc820b56ccc37e
Chris Burkert <burkert.chris@gmail.com>
2026-04-29 13:33:51 +0200

    fix: remove critical flags

--- internal/flags/flags.go
+++
@@ -1,8 +0,0 @@
-package flags
-
-const (
-	// DisableJWTTokenComputation disabling the existing JWT Handler, allowing the call to move forward
-	DisableJWTTokenComputation = "disable-jwt-token-computation"
-	// DisableClientCertificateComputation disabling the existing client certificates handler, allowing the call to move forward
-	DisableClientCertificateComputation = "disable-client-certificate-computation"
-)
